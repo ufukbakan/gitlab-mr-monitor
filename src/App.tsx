2c594b62a1aadaf97b929b5180d9f82ef9373f2b
ufukbakan <println.ufukbakan@gmail.com>
2023-06-09 13:18:49 +0300

    feat: render new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
-import "primereact/resources/primereact.min.css";  
+import "primeflex/primeflex.css";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primeicons/primeicons.css";
+import TopRightNav from "./ui/components/TopRightNav";
+import MergeRequestTable from "./ui/components/MergeRequestTable";
 
 function App() {
 
   return (
     <>
+      <div className="flex w-full p-4 justify-content-between">
+        <div className="top-left">
+          <img src="/gitlab.svg" id="logo" />
+          <h1 className="text-700">Merge Requests</h1>
+        </div>
 
+        <div className="top-right">
+          <TopRightNav />
+        </div>
+
+      </div>
+
+      <MergeRequestTable />
     </>
   )
 }
