318cd7d78a249164a956b90b481966b93937a522
ufukbakan <println.ufukbakan@gmail.com>
2023-06-09 13:17:36 +0300

    feat: ui components

---
+++ src/ui/components/MergeRequestTable.tsx
@@ -0,0 +1,47 @@
+import { Button } from "primereact/button";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { useRecoilValue } from "recoil";
+import { mergeRequestsAtom, projectsAtom } from "../../service/Commons";
+import { MergeRequest } from "../../service/types";
+import StatusCircle from "../elements/StatusCircle";
+
+export default function () {
+    const mergeRequests = useRecoilValue(mergeRequestsAtom);
+    const projects = useRecoilValue(projectsAtom);
+
+    return (
+        <DataTable value={mergeRequests}>
+            <Column
+                field="state"
+                header="State"
+                sortable
+                style={{ width: "2rem" }}
+                body={(mr: MergeRequest) => <StatusCircle status={mr.draft && mr.state != "closed" ? "draft" : mr.state} />}
+            />
+            <Column
+                field="project_id"
+                header="Project"
+                sortable
+                filter
+                body={(mr: MergeRequest) => projects.find(p => p.id == mr.project_id)?.name || mr.project_id}
+            />
+            <Column field="title" header="Title" sortable filter />
+            <Column field="author.name" header="Author" sortable filter />
+            <Column
+                field="target_brach"
+                header="Branch"
+                body={(mr: MergeRequest) => (
+                    <div className="flex align-items-center justify-content-start gap-2">
+                        <div>{mr.source_branch}</div> <i className="pi pi-arrow-right" /><div>{mr.target_branch}</div>
+                    </div>
+                )}
+            />
+            <Column
+                field="url"
+                header="Link"
+                body={(mr: MergeRequest) => <Button link icon="pi pi-external-link text-xl" onClick={() => open(mr.web_url, "_blank")} />}
+            />
+        </DataTable>
+    )
+}
---
+++ src/ui/components/TopRightNav.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { accessTokenAtom, branchesAtom, mergeRequestsAtom, mergeRequestStatesAtom, projectsAtom } from "../../service/Commons";
+import { fetchMergeRequests } from "../../service/MergeRequestService";
+import { fetchProjects } from "../../service/ProjectService";
+import AccessTokenInput from "../elements/AccessTokenInput";
+import BranchesDropdown from "../elements/BranchesDropdown";
+import StatesDropdown from "../elements/StatesDropdown";
+import { Button } from "primereact/button"
+import { MergeRequest, Project } from "../../service/types";
+
+export default function () {
+    const [isFetching, setIsFetching] = useState(false);
+    const setMergeRequests = useSetRecoilState(mergeRequestsAtom);
+    const setProjects = useSetRecoilState(projectsAtom);
+    const token = useRecoilValue(accessTokenAtom);
+    const branches = useRecoilValue(branchesAtom);
+    const states = useRecoilValue(mergeRequestStatesAtom);
+
+    async function fetchData() {
+        const promises = [fetchProjects(token), fetchMergeRequests(states, branches, token)];
+        return new Promise((resolve) => Promise.allSettled(promises).then((results) => {
+            const projectsResult = results[0];
+            const mergeRequestResult = results[1];
+            if(projectsResult.status == "fulfilled"){
+                setProjects(projectsResult.value as Project[]);
+            }
+            if(mergeRequestResult.status == "fulfilled"){
+                setMergeRequests(mergeRequestResult.value as MergeRequest[]);
+            }
+            resolve(null);
+        }));
+    }
+
+    async function loadData() {
+        setIsFetching(true);
+        await fetchData();
+        setIsFetching(false);
+    }
+
+    useEffect(() => {
+        loadData();
+        const interval = setInterval(loadData, 60000);
+        return () => {
+            clearInterval(interval);
+        }
+    }, [token, branches, states])
+
+    return (
+        <div className="flex gap-2">
+            <Button icon={isFetching ? "pi pi-spin pi-spinner" : "pi pi-replay"} disabled={isFetching} onClick={loadData} />
+            <AccessTokenInput />
+            <BranchesDropdown />
+            <StatesDropdown />
+        </div>
+    )
+}
