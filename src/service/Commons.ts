b432896167801582085fde5c62d9aad8341a44bf
ufukbakan <println.ufukbakan@gmail.com>
2023-06-09 15:58:04 +0300

    feat: helpful comment

--- src/service/Commons.ts
+++ src/service/Commons.ts
@@ -2,6 +2,7 @@ import { atom } from "recoil";
 import { MergeRequestState } from "../ui/elements/StatesDropdown";
 import { MergeRequest, Project } from "./types";
 
+// You may consider changing base url
 export const baseUrl = "https://gitlab.com/api/v4" as const;
 
 export const mergeRequestStatesAtom = atom<MergeRequestState[]>({
