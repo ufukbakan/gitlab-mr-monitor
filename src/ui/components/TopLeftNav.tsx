ff9129889b7408d5deddff5b106a86d8843b7351
ufukbakan <println.ufukbakan@gmail.com>
2023-06-09 16:01:09 +0300

    refactor: move top left nav elements

---
+++ src/ui/components/TopLeftNav.tsx
@@ -0,0 +1,8 @@
+export default function () {
+    return (
+        <>
+            <img src="/gitlab.svg" id="logo" />
+            <h1 className="text-700">Merge Requests</h1>
+        </>
+    )
+}
