a9e2f9e8f7cc37847f403356e093f33359cdd175
ufukbakan <println.ufukbakan@gmail.com>
2023-06-12 11:59:29 +0300

    feat: hook use inner width

---
+++ src/hooks/useInnerWidth.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function(){
+    const [innerWidth, setInnerWidth] = useState(globalThis.window.innerWidth);
+
+    function updateInnerWidth(){
+        setInnerWidth(globalThis.window.innerWidth);
+    }
+
+    useEffect(() => {
+        globalThis.window.addEventListener("resize", updateInnerWidth);
+        return () => globalThis.removeEventListener("resize", updateInnerWidth);
+    }, [])
+
+    return innerWidth;
+}
