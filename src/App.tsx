7e95344f46a59b8c0480505119b0418999ced5dc
Ufuk Bakan <println.ufukbakan@gmail.com>
2023-07-19 14:24:08 +0300

    feat: add oauth route

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
 import "primeflex/primeflex.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import TopRightNav from "./ui/components/TopRightNav";
-import MergeRequestTable from "./ui/components/MergeRequestTable";
-import TopLeftNav from "./ui/components/TopLeftNav";
-import HowTo from "./ui/components/HowTo";
+import { Route, Switch } from "wouter";
+import Home from "./ui/pages/Home";
+import Auth from "./ui/pages/Auth";
 
 function App() {
 
   return (
-    <>
-      <div className="flex w-full p-4 justify-content-between">
-        <div className="top-left">
-          <TopLeftNav />
-        </div>
-
-        <div className="top-right">
-          <TopRightNav />
-        </div>
-
-      </div>
-
-      <MergeRequestTable />
-      <HowTo />
-    </>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/oauth">
+        <Auth />
+      </Route>
+    </Switch>
   )
 }
 
