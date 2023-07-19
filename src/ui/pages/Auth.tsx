ed1813a45f7a7b74f3af05526b4d26db8a1caaa6
Ufuk Bakan <println.ufukbakan@gmail.com>
2023-07-19 14:59:49 +0300

    feat: new auth route

---
+++ src/ui/pages/Auth.tsx
@@ -0,0 +1,87 @@
+import axios, { AxiosResponse } from "axios";
+import { ProgressSpinner } from "primereact/progressspinner";
+import { ReactNode, useEffect, useState } from "react";
+import { authProvider, client_id, client_secret, redirect_uri, useAccessToken, useRefreshToken } from "../../service/Commons";
+import ThemeSwitcher from "../elements/ThemeSwitcher";
+
+interface AuthResponse {
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+    refresh_token: string;
+    scope: string;
+    created_at: number;
+}
+
+export default function () {
+    const searchParams = new URLSearchParams(globalThis.location.search);
+    const defaultMessage = "You will be redirected";
+    const [message, setMessage] = useState<ReactNode>(defaultMessage);
+    const { setRefreshToken } = useRefreshToken();
+    const { setAccessToken } = useAccessToken();
+
+    function handleTokenResponse(resp: AxiosResponse<AuthResponse>) {
+        setAccessToken(resp.data.access_token);
+        setRefreshToken(resp.data.refresh_token);
+        setTimeout(() => refreshAccessToken(resp.data.refresh_token), resp.data.expires_in * 1000);
+    }
+
+    function handleTokenError(err: any) {
+        console.error(err);
+        console.warn(err.message);
+        console.error("Couldn't refresh access token");
+    }
+
+    function refreshAccessToken(refresh_token: string) {
+        const body = {
+            client_id,
+            refresh_token,
+            grant_type: "refresh_token",
+            redirect_uri
+        }
+        const postUrl = `${authProvider}/oauth/token`;
+        axios.post<AuthResponse>(postUrl, body)
+            .then(handleTokenResponse)
+            .catch(handleTokenError);
+    }
+
+    function retrieveAccessToken() {
+        const body = {
+            client_id,
+            client_secret,
+            code: searchParams.get("code"),
+            grant_type: "authorization_code",
+            redirect_uri
+        }
+        const postUrl = `${authProvider}/oauth/token`;
+        axios.post<AuthResponse>(postUrl, body)
+            .then(resp => {
+                handleTokenResponse(resp);
+                globalThis.history.pushState(null, "", "/");
+            })
+            .catch(err => {
+                setMessage(
+                    <>
+                        <div>{err.message}</div>
+                        <div><a href="/">Return home</a></div>
+                    </>
+                );
+            });
+    }
+
+    useEffect(() => {
+        retrieveAccessToken();
+    }, []);
+
+    return (
+        <div className="h-fulls flex flex-column">
+            <div className="p-2">
+                <ThemeSwitcher className="" />
+            </div>
+            <div className="w-full h-full flex flex-grow-1 flex-column gap-4 justify-content-center align-items-center">
+                <ProgressSpinner aria-label="Loading" className={message != defaultMessage ? "hidden" : ""} />
+                <span>{message}</span>
+            </div>
+        </div>
+    )
+}
