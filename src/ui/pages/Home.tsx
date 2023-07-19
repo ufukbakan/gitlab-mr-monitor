4abbeee428ef771296ff2277909788d95e0e18b3
Ufuk Bakan <println.ufukbakan@gmail.com>
2023-07-19 15:00:14 +0300

    refactor: homepage moved here

---
+++ src/ui/pages/Home.tsx
@@ -0,0 +1,24 @@
+import HowTo from "../components/HowTo";
+import MergeRequestTable from "../components/MergeRequestTable";
+import TopLeftNav from "../components/TopLeftNav";
+import TopRightNav from "../components/TopRightNav";
+
+export default function () {
+    return (
+        <>
+            <div className="flex w-full p-4 justify-content-between">
+                <div className="top-left">
+                    <TopLeftNav />
+                </div>
+
+                <div className="top-right">
+                    <TopRightNav />
+                </div>
+
+            </div>
+
+            <MergeRequestTable />
+            <HowTo />
+        </>
+    )
+}
