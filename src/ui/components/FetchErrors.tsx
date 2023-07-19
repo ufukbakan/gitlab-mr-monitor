57107d77b9e6db5a838f0742f3cdb93881578d94
Ufuk Bakan <println.ufukbakan@gmail.com>
2023-07-19 22:02:26 +0300

    feat: add not logged in warning

--- src/ui/components/FetchErrors.tsx
+++ src/ui/components/FetchErrors.tsx
@@ -1,9 +1,10 @@
 import { useRecoilValue } from "recoil";
-import { fetchMrErrorAtom } from "../../service/Commons";
+import { fetchMrErrorAtom, useAccessToken } from "../../service/Commons";
 
 export default function () {
     let key = 0;
     const errors = useRecoilValue(fetchMrErrorAtom);
+    const { accessToken } = useAccessToken();
 
     function renderError(e: Error) {
         return (
@@ -14,5 +15,14 @@ export default function () {
         )
     }
 
-    return errors.map(renderError)
+    function renderLoginMessage() {
+        return (
+            <div className="text-yellow-500 white-space-pre flex flex-column gap-2 text-center">
+                <img src="/warning.svg" alt="warning-icon" height={100} className="opacity-50" />
+                <span>You should sign in or paste an access token to the input box which is placed at the top right corner</span>
+            </div>
+        )
+    }
+
+    return accessToken ? errors.map(renderError) : renderLoginMessage()
 }
