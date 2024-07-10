fd24837fcfce3adf4e2ac682bf37daeca78d06de
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-07-10 09:49:05 +0400

    feat: env variables for baseurl & auth provider, simplified redirect uri

--- src/service/Commons.ts
+++ src/service/Commons.ts
@@ -2,67 +2,66 @@ import { useEffect } from "react";
 import { atom, useRecoilState } from "recoil";
 import { MergeRequestState } from "../ui/elements/StatesDropdown";
 import { MergeRequest, Project } from "./types";
-const env = import.meta.env.MODE;
 
-// You may consider changing base url
-export const baseUrl = "https://gitlab.com/api/v4" as const;
-export const authProvider = "https://gitlab.com" as const;
+export const baseUrl = import.meta.env.VITE_API_URL;
+export const authProvider = import.meta.env.VITE_AUTH_PROVIDER;
+
 export const client_id = "bc7072f9dd9baeea3490d3d1b82c3dde271e443d3bdbebe89efe0d39118b48d1";
 export const client_secret = "46734d0c3f55dd12925e57f22a07efad38ac2c534885cdfee3e03ebbb4200a93";
-export const redirect_uri = env.toLowerCase().includes("dev") ? "http://localhost:5173/oauth" : "https://gitlab-mr-monitor.vercel.app/oauth";
+export const redirect_uri = location.origin.concat("/oauth");
 
 export const refreshJobAtom = atom<number>({
-    key: "refreshJob",
-    default: undefined
+  key: "refreshJob",
+  default: undefined,
 });
 
 export const fetchMrErrorAtom = atom<Error[]>({
-    key: "fetchMrError",
-    default: []
+  key: "fetchMrError",
+  default: [],
 });
 
 export const mergeRequestStatesAtom = atom<MergeRequestState[]>({
-    key: "selectedStates",
-    default: []
-})
+  key: "selectedStates",
+  default: [],
+});
 
 export const branchesAtom = atom<string[]>({
-    key: "selectedBranches",
-    default: []
+  key: "selectedBranches",
+  default: [],
 });
 
 const accessTokenAtom = atom<string>({
-    key: "accessToken",
-    default: localStorage.getItem("access-token") || ""
+  key: "accessToken",
+  default: localStorage.getItem("access-token") || "",
 });
 
 const refreshTokenAtom = atom<string>({
-    key: "refreshToken",
-    default: localStorage.getItem("refresh-token") || ""
+  key: "refreshToken",
+  default: localStorage.getItem("refresh-token") || "",
 });
 
 export const mergeRequestsAtom = atom<MergeRequest[]>({
-    key: "mergeRequests",
-    default: []
-})
+  key: "mergeRequests",
+  default: [],
+});
 
 export const projectsAtom = atom<Project[]>({
-    key: "projects",
-    default: []
-})
+  key: "projects",
+  default: [],
+});
 
 export const useAccessToken = () => {
-    const [accessToken, setAccessToken] = useRecoilState(accessTokenAtom);
-    useEffect(() => {
-        localStorage.setItem("access-token", accessToken);
-    }, [accessToken]);
-    return { accessToken, setAccessToken };
-}
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenAtom);
+  useEffect(() => {
+    localStorage.setItem("access-token", accessToken);
+  }, [accessToken]);
+  return { accessToken, setAccessToken };
+};
 
 export const useRefreshToken = () => {
-    const [token, setToken] = useRecoilState(refreshTokenAtom);
-    useEffect(() => {
-        localStorage.setItem("refresh-token", token);
-    }, [token]);
-    return { refreshToken: token, setRefreshToken: setToken };
-}
+  const [token, setToken] = useRecoilState(refreshTokenAtom);
+  useEffect(() => {
+    localStorage.setItem("refresh-token", token);
+  }, [token]);
+  return { refreshToken: token, setRefreshToken: setToken };
+};
