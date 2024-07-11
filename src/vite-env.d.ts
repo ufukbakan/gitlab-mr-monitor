2527b18d58677ed4fa8e36e5cf54af3fe337ee24
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-07-11 10:33:10 +0400

    feat: static typing for mr scope

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,12 @@
 /// <reference types="vite/client" />
 
+import { MergeRequestScope } from "./service/types";
+
 interface ImportMetaEnv {
     readonly BUILD_DIR: string;
     readonly VITE_API_URL: string;
     readonly VITE_AUTH_PROVIDER: string;
-    readonly VITE_MR_SCOPE: string;
+    readonly VITE_MR_SCOPE: MergeRequestScope;
     readonly VITE_DEFAULT_BRANCHES: string;
 }
 
